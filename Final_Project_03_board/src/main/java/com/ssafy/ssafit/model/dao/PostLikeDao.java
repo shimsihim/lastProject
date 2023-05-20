8aafd90e12ab3b888af44b88b313a5dcb0aeed7f
unknown <11goatmxj@naver.com>
2023-05-20 15:03:49 +0900

    0520 경섭 -v3

---
+++ Final_Project_03_board/src/main/java/com/ssafy/ssafit/model/dao/PostLikeDao.java
@@ -0,0 +1,11 @@
+package com.ssafy.ssafit.model.dao;
+
+import com.ssafy.ssafit.model.dto.PostLike;
+
+public interface PostLikeDao {
+	int checkPostLike(PostLike postlike);
+
+	void insertPostLike(PostLike postlike);
+
+	void deletePostLike(PostLike postlike);
+}
